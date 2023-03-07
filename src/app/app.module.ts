0ecfdfee8690be1608c8b706725f89978563660a
Naira Cirino <nacirino@gmail.com>
2023-03-07 17:49:55 -0300

    :heavy_plus_sign: [build] Configures packages and imports required modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+//Material
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+
+//Components
 import { AppComponent } from './app.component';
+import { NgforFunComponent } from './components/playing-with-directives/ngfor-fun/ngfor-fun.component';
+import { NgifFunComponent } from './components/playing-with-directives/ngif-fun/ngif-fun.component';
+import { NgstyleFunComponent } from './components/playing-with-directives/ngstyle-fun/ngstyle-fun.component';
+import { NgclassFunComponent } from './components/playing-with-directives/ngclass-fun/ngclass-fun.component';
+import { NgswitchFunComponent } from './components/playing-with-directives/ngswitch-fun/ngswitch-fun.component';
+import { NgtemplateFunComponent } from './components/playing-with-directives/ngtemplate-fun/ngtemplate-fun.component';
+import { NgcontainerFunComponent } from './components/playing-with-directives/ngcontainer-fun/ngcontainer-fun.component';
+import { NgcontentFunComponent } from './components/playing-with-directives/ngcontent-fun/ngcontent-fun.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgforFunComponent,
+    NgifFunComponent,
+    NgstyleFunComponent,
+    NgclassFunComponent,
+    NgswitchFunComponent,
+    NgtemplateFunComponent,
+    NgcontainerFunComponent,
+    NgcontentFunComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
