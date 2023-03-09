e1d0fc037ae073eebf7c7214f347657b62257e94
Naira Cirino <nacirino@gmail.com>
2023-03-09 16:07:49 -0300

    :wrench: [chore] Imports required modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NgModule } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
 
 //Components
 import { AppComponent } from './app.component';
@@ -37,7 +39,9 @@ import { NgcontentFunComponent } from './components/playing-with-directives/ngco
     MatSelectModule,
     MatFormFieldModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
