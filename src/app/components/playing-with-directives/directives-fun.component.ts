fef844eeb4cc567df6f977cf5c97fc5873661255
Naira Cirino <nacirino@gmail.com>
2023-03-11 11:49:16 -0300

    :sparkles: [feat] Creates a component to display other components

---
+++ src/app/components/playing-with-directives/directives-fun.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directives-fun',
+  templateUrl: './directives-fun.component.html',
+  styleUrls: ['./directives-fun.component.scss']
+})
+export class DirectivesFunComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
